85204794c1b5b34fc119fc0edf962847568b3458
ARVENTO\baris.avtan <baris.avtan@arvento.com>
2024-01-19 00:11:05 +0300

    login 4

--- src/App.js
+++ src/App.js
@@ -4,42 +4,9 @@ import Sidebar from "./Sidebar ";
 import Navbar from "./Navbar";
 import Login from "./Login";
  
-const TABLE_HEAD = ["Name", "Job", "Employed", ""];
- 
-const TABLE_ROWS = [
-  {
-    name: "John Michael",
-    job: "Manager",
-    date: "23/04/18",
-  },
-  {
-    name: "Alexa Liras",
-    job: "Developer",
-    date: "23/04/18",
-  },
-  {
-    name: "Laurent Perrier",
-    job: "Executive",
-    date: "19/09/17",
-  },
-  {
-    name: "Michael Levi",
-    job: "Developer",
-    date: "24/12/08",
-  },
-  {
-    name: "Richard Gran",
-    job: "Manager",
-    date: "04/10/21",
-  },
-];
- 
 function App() {
   return (
-
-  // <Home/>
   <>
-{/* <Login/> */}
   <Navbar/>
   <Sidebar />
   </>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,3 +1,5 @@
+
+
 import Sidebar from "./Sidebar ";
 import Navbar from "./Navbar";
 
