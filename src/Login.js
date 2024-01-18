527e33e88f65f42eb50fa6f1a1455532239e7e4e
ARVENTO\baris.avtan <baris.avtan@arvento.com>
2024-01-19 00:18:36 +0300

    son test

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import './App.css';
+import logo from './Images/logo2.jpg'; // Replace with the path to your logo
+
 
 function Login() {
     const navigate = useNavigate();
@@ -13,27 +15,31 @@ function Login() {
   return (
     
     <div className="App">
-            <div className="login-container">
-                <h2 style={{ marginBottom: '32px', color: '#4299e1' }}>Arvento CRM</h2>
-                <form className="login-form">
-                    <div className="form-input">
-                        <label htmlFor="username">Kullanıcı Adı</label>
-                        <input type="text" id="username" name="username" />
-                    </div>
-                    <div className="form-input">
-                        <label htmlFor="password">Şifre</label>
-                        <input type="password" id="password" name="password" />
-                    </div>
-
-                    <button type="submit" className="form-button" onClick={handleLogin}>
-                        Login
-                    </button>
-                </form>
-                <div className="forgot-password-link">
-                    <Link to="/ForgotPassword">Şifreni mi unuttun?</Link>
-                </div>
+    <div className="login-container">
+        {/* <img src={logo} alt="Arvento CRM Logo" className="logo" /> */}
+        <h2 style={{ marginBottom: '32px', color: '#4299e1',fontSize: '25px' }}>Arvento CRM</h2>
+        {/* <h2 style={{ marginBottom: '16px', color: '#4299e1', fontSize: '28px', fontWeight: 'bold' }}>
+                    Welcome to Arvento CRM
+                </h2> */}
+        <form className="login-form">
+            <div className="form-input">
+                <label htmlFor="username">Kullanıcı Adı</label>
+                <input type="text" id="username" name="username" />
             </div>
+            <div className="form-input">
+                <label htmlFor="password">Şifre</label>
+                <input type="password" id="password" name="password" />
+            </div>
+
+            <button type="submit" className="form-button" onClick={handleLogin}>
+                Login
+            </button>
+        </form>
+        <div className="forgot-password-link">
+            <Link to="/ForgotPassword">Şifreni mi unuttun?</Link>
         </div>
+    </div>
+</div>
   );
 }
 
